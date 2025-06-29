5ae43643093fcf6c4b38dbf274f8b09a7b2415ff
AlexeyMordovskoy <a.mordovskoy@gmail.com>
2025-06-29 23:20:33 +0300

    Firth commit

--- VariablesTheme.java
+++ VariablesTheme.java
@@ -5,6 +5,9 @@ import java.time.format.DateTimeFormatter;
 
 public class VariablesTheme {
     public static void main(String[] args) {
+        long nanoStartTime = System.nanoTime();
+        LocalTime localTimeStart = LocalTime.now();
+
         System.out.println("1. ВЫВОД ASCII-ГРАФИКИ");
         System.out.println("                     /\\");
         System.out.println("   J    a  v     v  /  \\");
@@ -13,12 +16,12 @@ public class VariablesTheme {
         System.out.println(" JJ  a     a  V  /___/\\___\\\n");
         
         System.out.printf("""
-                 /\\            
-           J    /  \\  v     v  a
-           J   /_( )\\  v   v  a a
-        J  J  /      \\  V V  aaaaa
-         JJ  /___/\\___\\  V  a     a
-        """);
+                         /\\            
+                   J    /  \\  v     v  a
+                   J   /_( )\\  v   v  a a
+                J  J  /      \\  V V  aaaaa
+                 JJ  /___/\\___\\  V  a     a
+                """);
 
         System.out.println("\n2. РАСЧЕТ СТОИМОСТИ ТОВАРА");
         System.out.println("Решение задачи без округления результата");
@@ -41,16 +44,15 @@ public class VariablesTheme {
         BigDecimal bookCostBd = new BigDecimal("235.23");
         BigDecimal discountBd = BigDecimal.valueOf(11);
 
-        var sumWithoutDiscountBd = penCostBd.add(bookCostBd);
-        var sumDiscount = sumWithoutDiscountBd.divide(new BigDecimal(100));
-        sumDiscount = sumDiscount.multiply(discountBd);
-        var sumWithDiscountBd = sumWithoutDiscountBd.subtract(sumDiscount).setScale(2, RoundingMode.HALF_UP);
-        var discountSumBd = (sumWithoutDiscountBd).subtract(sumWithDiscountBd);
+        //стоимость чека без скид
+        var noDiscountPriceBd = penCostBd.add(bookCostBd);
+        var discountAmount = ((noDiscountPriceBd.divide(BigDecimal.valueOf(100))).multiply(discountBd)).setScale(2, RoundingMode.HALF_UP);;
+        var discountPriceBd = (noDiscountPriceBd.subtract(discountAmount)).setScale(2, RoundingMode.HALF_UP);
 
         System.out.println("Стоимость ручки без скидки " + penCostBd);
         System.out.println("Стоимость книги без скидки " + bookCostBd);
-        System.out.println("Стоимость ручки и книги со скидкой " + sumWithDiscountBd);
-        System.out.println("Размер скидки " + discountSumBd);
+        System.out.println("Стоимость ручки и книги со скидкой " + discountPriceBd);
+        System.out.println("Размер скидки " + discountAmount);
 
         System.out.println("\n3. ПЕРЕСТАНОВКА ЗНАЧЕНИЙ ЯЧЕЕК В ТАБЛИЦЕ");
         System.out.println("Исходные значения A1 = 2, B1 = 5");
@@ -74,7 +76,6 @@ public class VariablesTheme {
         a1 ^= b1;
         System.out.println("Результат: A1 = " + a1 + ", B1 = " + b1);
         
-        // 4. ДЕКОДИРОВАНИЕ СООБЩЕНИЯ
         System.out.println("\n4. ДЕКОДИРОВАНИЕ СООБЩЕНИЯ");
 
         int code1 = 1055;
@@ -87,24 +88,22 @@ public class VariablesTheme {
         System.out.printf("%d%6d%6d%6d%6d%6d", code1, code2, code3, code4, code5, code6);
         System.out.printf("\n%c%6c%6c%6c%6c%6c", code1, code2, code3, code4, code5, code6);
 
-        // 5. АНАЛИЗ КОДА ТОВАРА
         System.out.println("\n\n5. АНАЛИЗ КОДА ТОВАРА");
         int productCode = 123;
-        int prodCategory1 = productCode / 100;
-        int prodCategory2 = (productCode / 10) % 10;
+        int prodCategory = productCode / 100;
+        int prodSubcategory = (productCode / 10) % 10;
         int prodPackageType = productCode % 10;
 
-        int checkSum = prodCategory1 + prodCategory2 + prodPackageType;
-        int checkcode = prodCategory1 * prodCategory2 * prodPackageType;
+        int checksum = prodCategory + prodSubcategory + prodPackageType;
+        int checkcode = prodCategory * prodSubcategory * prodPackageType;
 
         System.out.println("Код товара: " + productCode);
-        System.out.println("  категория товара - " + prodCategory1);
-        System.out.println("  подкатегория - " + prodCategory2);
+        System.out.println("  категория товара - " + prodCategory);
+        System.out.println("  подкатегория - " + prodSubcategory);
         System.out.println("  тип упаковки - " + prodPackageType);
-        System.out.println("Контрольная сумма = " + checkSum);
+        System.out.println("Контрольная сумма = " + checksum);
         System.out.println("Проверочный код = " + checkcode);
 
-        // 6. ТЕСТИРОВАНИЕ ДАТЧИКОВ ПЕРЕД ЗАПУСКОМ РАКЕТЫ
         System.out.println("\n6. ТЕСТИРОВАНИЕ ДАТЧИКОВ ПЕРЕД ЗАПУСКОМ РАКЕТЫ");
         byte temperature = Byte.MAX_VALUE;
         System.out.printf("""
@@ -146,14 +145,14 @@ public class VariablesTheme {
                 -1: %d
                 """.formatted(startTime, ++startTime, --startTime));
 
-        // 7. ВЫВОД ПАРАМЕТРОВ JVM И ОС
         System.out.println("7. ВЫВОД ПАРАМЕТРОВ JVM И ОС");
-        Runtime runtime = Runtime.getRuntime();
-        int numProcessor = runtime.availableProcessors();
-        long totalMemory = runtime.totalMemory() / 1024 / 1024;
-        long freeMemory = runtime.freeMemory() / 1024 / 1024;
+        Runtime rt = Runtime.getRuntime();
+        int divider = 1024 * 1024;
+        int cpuNum = rt.availableProcessors();
+        long totalMemory = rt.totalMemory() / divider;
+        long freeMemory = rt.freeMemory() / divider;
         long useMemory = totalMemory - freeMemory;
-        long maxMemory = runtime.maxMemory() / 1024 / 1024;
+        long maxMemory = rt.maxMemory() / divider;
 
         System.out.println("""
                 Доступное число ядер: %d
@@ -161,9 +160,10 @@ public class VariablesTheme {
                 Свободная память (Мб):  %d
                 Используемая память (Мб): %d
                 Максимально доступная для выделения память (Мб) %d
-                """.formatted(numProcessor, totalMemory, freeMemory, useMemory, maxMemory));
+                """.formatted(cpuNum, totalMemory, freeMemory, useMemory, maxMemory));
 
-        String systemDisk = System.getProperty("java.home");
+        String javaHome = System.getProperty("java.home");
+        String systemDisc = javaHome.substring(0,1);
         String versionOs = System.getProperty("os.name") + " version:" + System.getProperty("os.version");
         String versionJava = System.getProperty("java.version");
         String separator = System.getProperty("file.separator");
@@ -173,30 +173,17 @@ public class VariablesTheme {
                 Версия ОС: %s
                 Версия Java: %s
                 Символ разделения пути (сепаратор): %s
-                """.formatted(systemDisk, versionOs, versionJava, separator));
+                """.formatted(systemDisc, versionOs, versionJava, separator));
 
-        // 8. ЗАМЕР ВРЕМЕНИ РАБОТЫ КОДА
-        System.out.println("7. ВЫВОД ПАРАМЕТРОВ JVM И ОС");
-
-        long start = System.currentTimeMillis();
-        long finish = System.currentTimeMillis();
-
-        System.out.println(start);
-        System.out.println(finish);
-
-        // 8. ЗАМЕР ВРЕМЕНИ РАБОТЫ КОДА
-        long nanoStartTime = System.nanoTime();
-        LocalTime localTimeStart = LocalTime.now();
-
-        System.out.println("\n8. ЗАМЕР ВРЕМЕНИ РАБОТЫ КОДА");
+        System.out.println("8. ЗАМЕР ВРЕМЕНИ РАБОТЫ КОДА");
         long nanoTimeFinish = System.nanoTime();
         LocalTime localTimeFinish = LocalTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
 
-        double timeDif = (nanoTimeFinish - nanoStartTime) / 1000000000.0000;
-        BigDecimal rounded = new BigDecimal(timeDif).setScale(3, RoundingMode.UP);
+        double timeDif = (nanoTimeFinish - nanoStartTime) / 1e9;
+        double rounded = Math.round(timeDif * 1000.0) / 1000.0;
         System.out.println("Старт проверки: " + localTimeStart.format(formatter));
         System.out.println("Финиш проверки: " + localTimeFinish.format(formatter));
-        System.out.println("Время работы:   " + rounded + "сек");
+        System.out.println("Время работы:   " + rounded + " сек");
     }
 }
